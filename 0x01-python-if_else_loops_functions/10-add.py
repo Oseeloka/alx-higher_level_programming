c7ef4c12d710eff387c086ec897732771e02a746
oseeloka <oseeloka@gmail.com>
2022-12-01 19:17:03 -0800

    0x01-python-if_else_loops_functions

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author - Mayokun Kasumu
+def add(a, b):
+    """Return the addition of a and b."""
+    return (a + b)
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author -Mayokun Kasumu
+
+def pow(a, b):
+    return (a ** b)
